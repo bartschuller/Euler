bfae9e046c7a138ee5d5e5fab1f76a131a17b61b
Bart Schuller <schuller@lunatech.com>
2011-07-12 00:18:34 +0200

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,8 @@
+name := "Euler"
+
+version := "0.1.0-SNAPSHOT"
+
+organization := "org.smop"
+
+scalaVersion := "2.9.0-1"
+
---
+++ src/main/scala/Euler.scala
@@ -0,0 +1,18 @@
+
+object Euler {
+  def euler1 = (1 to 999).filter(x => x % 3 == 0 || x % 5 == 0).sum
+
+  def fib(n: Int): Int = {
+    def fibs(n: Int): (Int, Int) = n match {
+      case _ if n <= 0 => (0,1)
+      case _ => fibs(n-1) match {
+        case (a,b) => (b, a+b)
+      }
+    }
+    fibs(n)._1
+  }
+
+  def euler2 = Iterator.iterate((1L,2L))(p => (p._2, p._1+p._2)).map(_._1).takeWhile(_ < 4000000).filter(_ % 2 == 0).sum
+
+  
+}
